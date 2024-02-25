473e5af021158d20f8a07ec2a8dc90cf45f80a23
akanksha29-Jadhao <akankshajadhao79@gmail.com>
2024-02-25 12:37:23 +0530

    Initial Commit

---
+++ my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+
+
+// const h1=document.createElement('h1')
+// h1.innerHTML='I am learning React'
+
+// document.getElementById("root").appendChild()
+
+ReactDOM.render(
+ <h1>I am learning react js </h1>,
+  document.getElementById ("root"),
+  ()=>console.log("rendered")
+);
